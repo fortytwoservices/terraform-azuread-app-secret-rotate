341753756918bc85bfc1f9b34f514197a0284572
Ketil Gjerde <477141+mysteq@users.noreply.github.com>
2025-11-30 13:55:09 +0100

    format: add required versions
    Signed-off-by: Ketil Gjerde <477141+mysteq@users.noreply.github.com>

--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,5 @@
 terraform {
+  required_version = ">= 1.0.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -13,6 +14,7 @@ terraform {
     # }
     time = {
       source = "hashicorp/time"
+      version = ">=0.10.0"
     }
   }
 }
