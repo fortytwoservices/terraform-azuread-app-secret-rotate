8a35bacc64fb19a807f666a1f129bfca329ae963
Ketil Gjerde <477141+mysteq@users.noreply.github.com>
2025-12-20 19:01:01 +0100

    fix: correct example
    Signed-off-by: Ketil Gjerde <477141+mysteq@users.noreply.github.com>

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -39,14 +39,20 @@ resource "random_string" "kv_name" {
   upper   = false
 }
 
+#
 resource "azurerm_key_vault" "example" {
-  name                        = random_string.kv_name.result
-  location                    = azurerm_resource_group.example.location
-  resource_group_name         = azurerm_resource_group.example.name
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
+  name                       = random_string.kv_name.result
+  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.example.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days = 7
+  purge_protection_enabled   = true
+  rbac_authorization_enabled = true
+
+  network_acls {
+    default_action = "Deny"
+    bypass         = "None"
+  }
 
   sku_name = "standard"
 }
